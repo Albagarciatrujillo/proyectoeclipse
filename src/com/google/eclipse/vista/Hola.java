068483de71edb8256e9c294ab9852b6102b3f3d6
Alba García <agartru2003@g.educaand.es>
2024-10-29 10:41:16 +0100

    cambios en master

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -3,7 +3,9 @@ package com.google.eclipse.vista;
 public class Hola {
 	
 	public static void main(String[]args) {
-		System.out.println("Hola mundo!!!");
+		System.out.println("Hoooooooooooooola mundo!!!");
+		System.out.println("Hoooooooooooooola mundo!!!");
+		System.out.println("Hoooooooooooooola mundo!!!");
 	}
 
 }
