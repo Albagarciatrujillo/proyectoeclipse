94fcb9c200dc0b6bc71ba3592ecdeaa5a91bddb0
Alba García <agartru2003@g.educaand.es>
2024-11-05 10:05:40 +0100

    otro cambio en adios

--- src/com/google/eclipse/vista/Adios.java
+++ src/com/google/eclipse/vista/Adios.java
@@ -3,5 +3,7 @@ package com.google.eclipse.vista;
 public class Adios {
 	public static void main(String[] args) {
 		System.out.println("Adios!!!!!");
+		System.out.println("Adios!!!!!");
+		System.out.println("Adios!!!!!");
 	}
 }
